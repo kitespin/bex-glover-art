4ece6aaeb1f7afa528073aaee2c217ef30f3f71b
Paul Pritchard <makerpaul@gmail.com>
2019-08-16 15:10:59 +0100

    added custom post to add latest news to front page in functions.php and front-page.php

--- front-page.php
+++ front-page.php
@@ -9,24 +9,55 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
 
-			<?php while ( have_posts() ) : the_post(); ?>
+		<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content', 'front' ); ?>
+			<?php get_template_part( 'template-parts/content', 'front' ); ?>
 
-				<?php
+
+			<?php
 					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-				?>
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+		endif;
+		?>
 
-			<?php endwhile; // End of the loop. ?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+	<?php endwhile; // End of the loop. ?>
+
+
+	<!--output of query result in functions.php-->
+	<!--
+	<h3>Todays posts</h3>
+	<?php
+	//while ( $result->have_posts() ) : $result->the_post();      
+		?>
+		<h4><?php //the_title(); ?></h4>
+		<?php
+	//endwhile; 
+	//wp_reset_query();
+	?>
+-->
+	<h4>Todays News</h4>
+	<?php
+	if ( $result->have_posts() ){
+	$result->the_post();  
+		?>
+		<a href="http://192.168.33.11/news/" ><h5 style="color:orange;"><?php the_title(); ?></h5></a>
+		<?php
+	}else{
+		echo "No posts";
+	}
+	
+	wp_reset_query();
+	?>
+
+
+
+</main><!-- #main -->
+</div><!-- #primary -->
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
--- functions.php
+++ functions.php
@@ -4,3 +4,19 @@ add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles');
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
+
+
+// query for getting post only from today
+
+$today = getdate();
+$query_args = array(
+	'date_query' => array(
+		array(
+			'year' => $today['year'],
+			'month' => $today['mon'],
+			'day' => $today['mday'],
+		),
+	),
+);
+
+$result = new WP_Query($query_args);
